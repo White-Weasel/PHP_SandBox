3c7648d35c6f65d20a999194d95128999a11dce3
Magic-cat-No765 <Magic-cat-No765@users.noreply.github.com>
2021-10-28 21:01:17 +0700

    debugging

--- PHP/LT3/test_imgur.php
+++ PHP/LT3/test_imgur.php
@@ -13,12 +13,10 @@
    <?php
       if (isset($_POST['submited']) && isset($_FILES))
       {
-         $target_dir = /*$_SERVER['DOCUMENT_ROOT'].*/"/src/img/";
-         $target_file = $target_dir . basename($_FILES["fileToUpload"]["tmp_name"]);
+         $target_dir = $_SERVER['DOCUMENT_ROOT']."/src/img/";
+         $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
          move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
-
-         $imgur = new Imgur();
-         echo $imgur->upload_image($target_file);
+         echo $imgur->upload_image($file);
       }
    ?>
 </body>
