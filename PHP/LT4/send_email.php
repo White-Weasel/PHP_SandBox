72d6b4c5ac733c1e708f34604b70407e291fb655
Magic-cat-No765 <Magic-cat-No765@users.noreply.github.com>
2021-11-03 22:30:32 +0700

    make it better

--- PHP/LT4/send_email.php
+++ PHP/LT4/send_email.php
@@ -15,13 +15,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="/src/site.css">
-    <title>Đăng nhập</title>
+    <link rel="stylesheet" type="text/css" href="/src/site.css">    
+    <title>Email</title>
 </head>
 <body>
     <div class="flex justify-content-center">
         <form class="flex flex-dir-col" method="POST" style="width: 355px;">
-            <h1 class="text-center">Đăng nhập</h1>
+            <h1 class="text-center">Gửi email xác nhận tới</h1>
             <input class="input-field" type="text" name="email" placeholder="Gửi tới email ..." required style="margin-bottom: 10px;">
             <input class="btn btn-blue" name="submited" type="submit" value="Gửi" required style="margin-bottom: 10px;">
             <div>Chưa có tài khoản?<a class="text-bold" href="/PHP/LT2/signup.php" style="margin-left:5px">Đăng ký</a></div>
